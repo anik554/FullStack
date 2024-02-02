104d87f5919718771a4b23233d32afc40babde2d
anik924 <anikimtiaz924@gmail.com>
2024-02-03 01:50:42 +0600

    L-1 Local module export and import

---
+++ LocalModules/index.js
@@ -0,0 +1,14 @@
+const importFunction = require('./student') // import all function 
+
+const {getName} = require('./student')  // indivisial function import
+console.log(getName());
+
+// console.log(importFunction.Name())
+// console.log(importFunction.Age())
+// console.log(importFunction.Result())
+
+//or
+// console.log(importFunction.getName())
+// console.log(importFunction.getAge())
+// console.log(importFunction.result())
+
---
+++ LocalModules/student.js
@@ -0,0 +1,22 @@
+exports.getName =()=>{  // export indivisual function
+    return " My name is Anik"
+}
+
+exports.getAge =()=>{
+    return 30
+}
+
+const result = ()=>{
+    return "A+"
+}
+
+// exports.Name = getName;
+// exports.Age = getAge;
+// exports.Result = result;
+
+//or
+
+// module.exports={
+//     getName,getAge,result
+// }
+
